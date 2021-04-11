eabb2673b544efd18e958863ea7b5c753c4099e9
Tyler Decarie <tylerjdecarie@gmail.com>
2021-04-11 00:34:39 -0600

    prefix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = "!";
+
 client.on("message", function(message) { 
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
