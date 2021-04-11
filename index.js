1f4c0e969f1e2d17f3277743496b29a3234ce237
Tyler Decarie <tylerjdecarie@gmail.com>
2021-04-11 00:02:15 -0600

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+client.on('message', msg => {
+  msg.reply('pong');
+});
+
+client.login('ODMwNjgwOTc5NzY0NTQzNDkx.YHKN8g.n1_HZ_SI44YcfzSjk0u4amyIksM');
